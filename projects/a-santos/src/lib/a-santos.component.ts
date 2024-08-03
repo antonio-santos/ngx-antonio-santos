f8fa524c907f8a0f8a8eba681583fcf332a09cf4
António Santos <antonio.santos@zact.com>
2024-08-03 20:54:52 +0100

    feat: Add Angular Material

--- projects/a-santos/src/lib/a-santos.component.ts
+++ projects/a-santos/src/lib/a-santos.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'as-a-santos',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   template: `
-    <p>
-      a-santos works!
-    </p>
+    <p>a-santos works!</p>
+    <button mat-flat-button color="primary">Primary</button>
   `,
-  styles: ``
+  styles: ``,
 })
-export class ASantosComponent {
-
-}
+export class ASantosComponent {}
